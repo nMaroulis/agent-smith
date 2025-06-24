9274134c630b85df0d6d9d5474710c586985b66e
Nikolaos Maroulis <nick.maroulis@hotmail.com>
2025-06-24 15:04:08 +0200

    toolbar add state, restructure

--- frontend/src/components/FlowCanvas.tsx
+++ frontend/src/components/FlowCanvas.tsx
@@ -415,18 +415,18 @@ export const FlowCanvas: React.FC<FlowCanvasProps> = ({
               <div className="absolute -bottom-1 left-1/2 transform -translate-x-1/2 w-1 h-1 bg-emerald-400 rounded-full opacity-0 group-hover:opacity-100 transition-opacity duration-200"></div>
             </button>
 
-            {/* LLM Node */}
+            {/* Node */}
             <button 
               className="flex flex-col items-center justify-center w-16 h-16 mx-1 rounded-full transition-all duration-200 hover:bg-gray-700/50 group relative"
               onClick={() => handleAddNode('llm')}
-              title="Add LLM Node"
+              title="Add Node"
             >
               <div className="bg-blue-500/20 p-3 rounded-full group-hover:bg-blue-500/30 transition-colors duration-200">
                 <svg className="w-5 h-5 text-blue-400 group-hover:text-blue-300 transition-colors duration-200" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                 </svg>
               </div>
-              <span className="text-xs mt-1 text-gray-300 group-hover:text-white transition-colors duration-200">LLM</span>
+              <span className="text-xs mt-1 text-gray-300 group-hover:text-white transition-colors duration-200">Node</span>
               <div className="absolute -bottom-1 left-1/2 transform -translate-x-1/2 w-1 h-1 bg-blue-400 rounded-full opacity-0 group-hover:opacity-100 transition-opacity duration-200"></div>
             </button>
 
@@ -446,7 +446,6 @@ export const FlowCanvas: React.FC<FlowCanvasProps> = ({
             </button>
 
             {/* End Node */}
-            <div className="h-8 w-px bg-gray-600/50 mx-1"></div>
             <button 
               className="flex flex-col items-center justify-center w-16 h-16 mx-1 rounded-full transition-all duration-200 hover:bg-gray-700/50 group relative"
               onClick={() => handleAddNode('end')}
@@ -460,6 +459,24 @@ export const FlowCanvas: React.FC<FlowCanvasProps> = ({
               </div>
               <span className="text-xs mt-1 text-gray-300 group-hover:text-white transition-colors duration-200">End</span>
               <div className="absolute -bottom-1 left-1/2 transform -translate-x-1/2 w-1 h-1 bg-rose-400 rounded-full opacity-0 group-hover:opacity-100 transition-opacity duration-200"></div>
+            </button>
+
+            {/* Divider */}
+            <div className="h-8 w-px bg-gray-600/50 mx-1"></div>
+
+            {/* State Tool */}
+            <button 
+              className="flex flex-col items-center justify-center w-16 h-16 mx-1 rounded-full transition-all duration-200 hover:bg-gray-700/50 group relative"
+              onClick={() => console.log('State tool clicked')}
+              title="State"
+            >
+              <div className="bg-amber-500/20 p-3 rounded-full group-hover:bg-amber-500/30 transition-colors duration-200">
+                <svg className="w-5 h-5 text-amber-400 group-hover:text-amber-300 transition-colors duration-200" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 7v10c0 2.21 3.582 4 8 4s8-1.79 8-4V7M4 7c0 2.21 3.582 4 8 4s8-1.79 8-4M4 7c0-2.21 3.582-4 8-4s8 1.79 8 4" />
+                </svg>
+              </div>
+              <span className="text-xs mt-1 text-gray-300 group-hover:text-white transition-colors duration-200">State</span>
+              <div className="absolute -bottom-1 left-1/2 transform -translate-x-1/2 w-1 h-1 bg-amber-400 rounded-full opacity-0 group-hover:opacity-100 transition-opacity duration-200"></div>
             </button>
           </div>
         </div>
